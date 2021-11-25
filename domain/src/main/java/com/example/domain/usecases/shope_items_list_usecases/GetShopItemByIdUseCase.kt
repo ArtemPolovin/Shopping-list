92798ff30214dabd6046f33eeb3dea9e26bd0280
Artem_Polovin <artem@cazton.com>
2021-11-24 20:54:29 -0800

    [task-1] created shop list items logic use cases

---
+++ domain/src/main/java/com/example/domain/usecases/shope_items_list_usecases/GetShopItemByIdUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.domain.usecases.shope_items_list_usecases
+
+import com.example.domain.models.ShopItem
+
+class GetShopItemByIdUseCase {
+
+    fun getShopItemById(shopItemId: Int): ShopItem {
+        TODO()
+    }
+}
